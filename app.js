25d338ac02f54c55fcb1197caf368e469ab4cc00
davdmoo <david.mulyawan97@gmail.com>
2022-07-23 15:58:35 +0700

    Postgresql setup & db create

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-const fastify = require('fastify')({
-  logger: true
-})
+const fastify = require('fastify')({ logger: true });
+const routes = require("./routes/index");
 
-fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
-})
+fastify.register(routes);
 
-/**
- * Run the server!
- */
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 3000 });
   } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
+    fastify.log.error(err);
+    process.exit(1);
   }
 }
-start()
+
+start();
